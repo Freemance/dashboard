68ddbbe0ef296e958994074574896e2557ad4bc8
paul <paul@outboss.io>
2021-08-23 17:08:38 -0400

    fix:first commit

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,30 @@
-const CracoAlias = require("craco-alias");
+const CracoAlias = require('craco-alias');
 
 module.exports = {
   plugins: [
     {
       plugin: CracoAlias,
       options: {
-        source: "tsconfig",
+        source: 'tsconfig',
         // baseUrl SHOULD be specified
         // plugin does not take it from tsconfig
-        baseUrl: "./src",
+        baseUrl: './src',
         /* tsConfigPath should point to the file where "baseUrl" and "paths" 
              are specified*/
-        tsConfigPath: "./tsconfig.paths.json",
+        tsConfigPath: './tsconfig.paths.json',
 
-        extends: "./tsconfig.paths.json",
+        extends: './tsconfig.paths.json',
       },
     },
   ],
+  eslint: {
+    enable: false /* (default value) */,
+    mode: 'file',
+    configure: (eslintConfig, { env, paths }) => {
+      return eslintConfig;
+    },
+    pluginOptions: (eslintOptions, { env, paths }) => {
+      return eslintOptions;
+    },
+  },
 };
