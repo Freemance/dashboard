988f77e161172358be019292fd88c4d3b4ff9082
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-08-25 23:41:16 -0400

    adding the login page form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,22 +2,21 @@ import React, { useState } from 'react';
 import {
   OutlinedInput,
   Container,
-  // CssBaseline,
   InputLabel,
-  // Checkbox,
   makeStyles,
-  // Grid,
   Button,
   FormControlLabel,
   Checkbox,
   InputAdornment,
   IconButton,
+  Box,
 } from '@material-ui/core';
+import Facebook from '@material-ui/icons/Facebook';
+import Twitter from '@material-ui/icons/Twitter';
+import EmailOutlined from '@material-ui/icons/EmailOutlined';
+import GitHub from '@material-ui/icons/GitHub';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
-// import { Link } from 'react-router-dom'
-// import { useHistory } from 'react-router-dom'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { auth } from 'store/actions'
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -27,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
   },
   inputSpace: {
-    marginBottom: '5vh',
+    marginBottom: '1rem',
   },
   labelSpace: {
     marginBottom: '1vh',
@@ -35,13 +34,42 @@ const useStyles = makeStyles((theme) => ({
   forgotPasswordDiv: {
     display: 'flex',
     justifyContent: 'space-between',
-  },
-  forgotPassword: {
-    margin: '0 0 1vh, 0',
+    alignItems: 'center',
   },
   form: {
     width: '100%',
     marginTop: theme.spacing(1),
+  },
+  link: {
+    margin: theme.spacing(0.5),
+    textDecoration: 'none',
+  },
+  checkbox: {
+    marginBottom: '0.5vh',
+    marginTop: '0.5vh',
+  },
+  divider: {
+    margin: theme.spacing(0, 0),
+    width: '100%',
+    textAlign: 'center',
+    color: 'black',
+  },
+  dividerPara: {
+    borderBottom: `1px solid grey`,
+    lineHeight: '0.1em',
+    margin: '0.5rem 0 1rem 0',
+    padding: '0',
+  },
+  dividerSpan: {
+    backgroundColor: 'white',
+    padding: '0 5px 0 5px',
+  },
+  icon: {
+    cursor: 'pointer',
+    margin: theme.spacing(0, 2),
+  },
+  registerLink: {
+    margin: '1rem 0',
   },
 }));
 
@@ -58,30 +86,11 @@ const LoginPage = () => {
       [name]: value,
     }));
   };
-  // const history = useHistory()
-  // const dispatch = useDispatch()
-  // const { isAuthenticated, loading, error } = useSelector((state) => state.auth)
-
-  // const authUser = (event) => {
-  //     event.preventDefault()
-  //     dispatch(auth(email, password, history))
-  // }
-
-  // useEffect(() => {
-  //     if (isAuthenticated) {
-  //         history.push("/dashboard")
-  //     }
-  // }, [isAuthenticated, history])
 
   return (
     <Container component="main" maxWidth="xs">
-      {/* <CssBaseline /> */}
       <div className={classes.paper}>
-        <form
-          className={classes.form}
-          noValidate
-          // onSubmit={login}
-        >
+        <form className={classes.form} noValidate>
           <div>
             <InputLabel className={classes.labelSpace} htmlFor="email">
               Email
@@ -91,7 +100,6 @@ const LoginPage = () => {
               required
               fullWidth
               id="email"
-              // label="Email Address"
               name="email"
               autoComplete="email"
               placeholder="Enter your email"
@@ -102,16 +110,15 @@ const LoginPage = () => {
           </div>
           <div>
             <div className={classes.forgotPasswordDiv}>
-              <InputLabel className={classes.labelSpace} htmlFor="password">
-                Password
-              </InputLabel>
-              <a className={classes.forgotPassword}>Forgot Password?</a>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Link to="/forgot_password" className={classes.link}>
+                Forgot Password?
+              </Link>
             </div>
             <OutlinedInput
               required
               fullWidth
               name="password"
-              // label="Password"
               type="password"
               id="password"
               placeholder="Enter your password"
@@ -122,8 +129,6 @@ const LoginPage = () => {
                 <InputAdornment position="end">
                   <IconButton
                     aria-label="toggle password visibility"
-                    // onClick={handleClickShowPassword}
-                    // onMouseDown={handleMouseDownPassword}
                     edge="end"
                   >
                     {true ? <Visibility /> : <VisibilityOff />}
@@ -133,6 +138,7 @@ const LoginPage = () => {
             />
           </div>
           <FormControlLabel
+            className={classes.checkbox}
             control={
               <Checkbox
                 // checked={state.checkedB}
@@ -143,23 +149,34 @@ const LoginPage = () => {
             }
             label="Remember Me"
           />
-          {/* <Grid>
-                        <Checkbox/>
-                    </Grid> */}
+
           <Button
             // isLoading={loading}
             variant="contained"
             color="primary"
-            // caption="SignIn"
             fullWidth
           >
             Sign In
           </Button>
         </form>
+        <p className={classes.registerLink}>
+          New on our platform?{' '}
+          <Link className={classes.link} to="/signup">
+            Create an account
+          </Link>
+        </p>
+        <div className={classes.divider}>
+          <p className={classes.dividerPara}>
+            <span className={classes.dividerSpan}>or</span>
+          </p>
+        </div>
+        <Box mt={1}>
+          <Facebook color="primary" className={classes.icon} />
+          <Twitter color="primary" className={classes.icon} />
+          <EmailOutlined color="primary" className={classes.icon} />
+          <GitHub color="primary" className={classes.icon} />
+        </Box>
       </div>
-      {/* <Box mt={4}>
-                <Copyright />
-            </Box> */}
     </Container>
   );
 };
