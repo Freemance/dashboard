8c3502128bffc5b29b27b06a48e7f58741bc94fe
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-08-30 10:02:19 -0400

    removing login form to a separated component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,89 +1,25 @@
 import React, { useState } from 'react';
-import {
-  OutlinedInput,
-  Container,
-  InputLabel,
-  makeStyles,
-  Button,
-  FormControlLabel,
-  Checkbox,
-  InputAdornment,
-  IconButton,
-  Box,
-} from '@material-ui/core';
+import { Container, Box } from '@material-ui/core';
 import Facebook from '@material-ui/icons/Facebook';
 import Twitter from '@material-ui/icons/Twitter';
 import EmailOutlined from '@material-ui/icons/EmailOutlined';
 import GitHub from '@material-ui/icons/GitHub';
-import { Visibility, VisibilityOff } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  inputSpace: {
-    marginBottom: '1rem',
-  },
-  labelSpace: {
-    marginBottom: '1vh',
-  },
-  forgotPasswordDiv: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
-  },
-  link: {
-    margin: theme.spacing(0.5),
-    textDecoration: 'none',
-  },
-  checkbox: {
-    marginBottom: '0.5vh',
-    marginTop: '0.5vh',
-  },
-  divider: {
-    margin: theme.spacing(0, 0),
-    width: '100%',
-    textAlign: 'center',
-    color: 'black',
-  },
-  dividerPara: {
-    borderBottom: `1px solid grey`,
-    lineHeight: '0.1em',
-    margin: '0.5rem 0 1rem 0',
-    padding: '0',
-  },
-  dividerSpan: {
-    backgroundColor: 'white',
-    padding: '0 5px 0 5px',
-  },
-  icon: {
-    cursor: 'pointer',
-    margin: theme.spacing(0, 2),
-  },
-  registerLink: {
-    margin: '1rem 0',
-  },
-}));
+import LoginForm from 'components/forms/LoginForm/LoginForm';
+import { useStyles } from 'pages/LoginPage/LoginPage.styles';
 
 const LoginPage = () => {
+  const classes = useStyles();
   const [formValues, setFormValues] = useState({
     email: '',
     password: '',
   });
-  const [state, setState] = useState({
+  const [formState, setFormState] = useState({
     passwordVisible: false,
     checked: false,
   });
-  const classes = useStyles();
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleFormInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormValues((stateValue) => ({
       ...stateValue,
@@ -91,15 +27,15 @@ const LoginPage = () => {
     }));
   };
 
-  const toggleVisibility = () => {
-    setState((state) => ({
+  const togglePasswordVisibility = () => {
+    setFormState((state) => ({
       ...state,
-      passwordVisible: !state.passwordVisible,
+      passwordVisible: !formState.passwordVisible,
     }));
   };
 
-  const toggleChecked = () => {
-    setState((state) => ({
+  const toggleCheckboxState = () => {
+    setFormState((state) => ({
       ...state,
       checked: !state.checked,
     }));
@@ -108,76 +44,13 @@ const LoginPage = () => {
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
-        <form className={classes.form} noValidate>
-          <div>
-            <InputLabel className={classes.labelSpace} htmlFor="email">
-              Email
-            </InputLabel>
-            <OutlinedInput
-              className={classes.inputSpace}
-              required
-              fullWidth
-              id="email"
-              name="email"
-              autoComplete="email"
-              placeholder="Enter your email"
-              autoFocus
-              value={formValues.email}
-              onChange={handleInput}
-            />
-          </div>
-          <div>
-            <div className={classes.forgotPasswordDiv}>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Link to="/forgot_password" className={classes.link}>
-                Forgot Password?
-              </Link>
-            </div>
-            <OutlinedInput
-              required
-              fullWidth
-              name="password"
-              type={state.passwordVisible ? 'text' : 'password'}
-              id="password"
-              placeholder="Enter your password"
-              autoComplete="current-password"
-              value={formValues.password}
-              onChange={handleInput}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={toggleVisibility}
-                    aria-label="toggle password visibility"
-                    edge="end"
-                  >
-                    {state.passwordVisible ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </div>
-          <FormControlLabel
-            className={classes.checkbox}
-            control={
-              <Checkbox
-                checked={state.checked}
-                onClick={toggleChecked}
-                name="rememberMe"
-                color="primary"
-              />
-            }
-            label="Remember Me"
-          />
-
-          <Button
-            // isLoading={loading}
-            variant="contained"
-            color="primary"
-            fullWidth
-          >
-            Sign In
-          </Button>
-        </form>
+        <LoginForm
+          formValues={formValues}
+          formState={formState}
+          handleFormInput={handleFormInput}
+          togglePasswordVisibility={togglePasswordVisibility}
+          toggleCheckboxState={toggleCheckboxState}
+        />
         <p className={classes.registerLink}>
           New on our platform?{' '}
           <Link className={classes.link} to="/signup">
